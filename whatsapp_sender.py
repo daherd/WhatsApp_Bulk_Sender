a3a7b2259eda4576ad41050de0d666f6ca9c9a01
daherd <daherda99@gmail.com>
2024-06-29 16:50:36 +0300

    Your commit message

--- whatsapp_sender.py
+++ whatsapp_sender.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 
+
+
+
 def run_webdriver(excel_file_path, massage):
     wb = load_workbook(excel_file_path)
     ws = wb.active
@@ -35,21 +38,16 @@ def run_webdriver(excel_file_path, massage):
             else:
                 web.implicitly_wait(3)
 
-            try:
-                sleep(4)
-                web.find_element(By.XPATH,
-                                 '/html/body/div[1]/div/div[2]/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(massage)
-            except:
-                web.implicitly_wait(1)
 
             try:
                 sleep(4)
                 web.find_element(By.XPATH,
-                                 '/html/body/div[1]/div/div[2]/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]').click()
+                                 '/html/body/div[1]/div/div[2]/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button/span').click()
             except:
                 web.implicitly_wait(1)
             sleep(3)
     sleep(10)
     web.close()
 
-#run_webdriver("Book1.xlsx","HI")
+
+run_webdriver("Book1.xlsx","%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85+%D8%B9%D9%84%D9%8A%D9%83%D9%85+%D8%A7%D8%AE%D9%8A+%D8%A7%D9%84%D9%83%D8%B1%D9%8A%D9%85%0D%0A%D9%86%D8%B0%D9%83%D8%B1%D9%83+%D8%A8%D8%B4%D8%AF+%D8%A7%D9%84%D8%B1%D8%AD%D8%A7%D9%84+%D8%A7%D9%84%D9%89+%D8%A7%D9%84%D9%85%D8%B3%D8%AC%D8%AF+%D8%A7%D9%84%D8%A3%D9%82%D8%B5%D9%89+%D8%BA%D8%AF%D8%A7+%D8%A5%D9%86+%D8%B4%D8%A7%D8%A1+%D8%A7%D9%84%D9%84%D9%87.%0D%0A%D8%A7%D9%84%D8%A7%D9%86%D8%B7%D9%84%D8%A7%D9%82+%D9%85%D9%86+%D8%A7%D9%85%D8%A7%D9%85+%D8%A8%D9%88%D8%A7%D8%A8%D8%A9+5+%D8%A7%D9%84%D8%B3%D8%A7%D8%B9%D8%A9+15%3A00.%0D%0A%D8%A7%D9%84%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC+%D9%8A%D8%B4%D9%85%D9%84+%D8%A7%D9%81%D8%B7%D8%A7%D8%B1+%D8%AC%D9%85%D8%A7%D8%B9%D9%8A+%D9%88%D8%B5%D9%84%D8%A7%D8%A9+%D8%AA%D8%B1%D8%A7%D9%88%D9%8A%D8%AD%0D%0A%D8%A7%D9%84%D8%B1%D8%AC%D8%A7%D8%A1+%D8%A7%D9%84%D8%A7%D9%84%D8%AA%D8%B2%D8%A7%D9%85+%D8%A8%D8%A7%D9%84%D9%88%D9%82%D8%AA%0D%0A%D9%86%D8%B1%D8%A7%D9%83+%D8%B9%D9%84%D9%89+%D8%AE%D9%8A%D8%B1+%0D%0A%0D%0A%D8%AA%D8%A3%D9%83%D9%8A%D8%AF+%D8%A7%D9%84%D8%AD%D8%B6%D9%88%D8%B1+%D9%85%D9%86+%D8%AE%D9%84%D8%A7%D9%84+%D8%A7%D9%84%D8%B1%D8%AF+%D8%B9%D9%84%D9%89+%D8%A7%D9%84%D8%B1%D8%B3%D8%A7%D9%84%D8%A9.")
